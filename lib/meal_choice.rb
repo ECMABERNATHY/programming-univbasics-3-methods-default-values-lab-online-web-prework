007250876beea18cb023683bf7d72a667e7756c1
Erin Abernathy <ecmabernathy@gmail.com>
2019-08-28 23:11:05 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -1,1 +1,9 @@
-# Your code here
+expect(meal_choice("asparagus", "corn")).to eq( "meat with asparagus and corn.")
+  end
+  
+    expect($stdout).to receive(:puts).with("What a nutritious meal!")
+    expect($stdout).to receive(:puts).with(" meat with asparagus and corn.")
+    meal_choice("asparagus", "corn")
+  end
+end 
+
